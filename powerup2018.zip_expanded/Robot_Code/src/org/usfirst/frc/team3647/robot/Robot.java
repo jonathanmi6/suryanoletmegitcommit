c23e82c831701e56ce10626ea40874b2dbcb2eb2
Surya Madhan <suryamadhan@gmail.com>
2018-08-17 19:54:53 -0700

    yeet that code over

---
+++ powerup2018.zip_expanded/Robot_Code/src/org/usfirst/frc/team3647/robot/Robot.java
@@ -0,0 +1,303 @@
+package org.usfirst.frc.team3647.robot;
+
+import edu.wpi.first.wpilibj.CameraServer;
+import edu.wpi.first.wpilibj.DoubleSolenoid;
+import edu.wpi.first.wpilibj.DriverStation;
+import edu.wpi.first.wpilibj.IterativeRobot;
+import edu.wpi.first.wpilibj.MotorSafety;
+import edu.wpi.first.wpilibj.MotorSafetyHelper;
+import edu.wpi.first.wpilibj.Timer;
+import team3647elevator.Elevator;
+import team3647elevator.ElevatorLevel;
+import team3647elevator.IntakeWheels;
+import team3647elevator.Wrist;
+import team3647pistons.Intake;
+import team3647pistons.Lock;
+import team3647pistons.Compressor007;
+import team3647pistons.Forks;
+import team3647pistons.Shifter;
+import team3647subsystems.Drivetrain;
+import team3647subsystems.Encoders;
+import team3647subsystems.Joysticks;
+import team3647subsystems.Lights;
+import team3647subsystems.TiltServo;
+
+
+public class Robot extends IterativeRobot 
+{
+	//Objects
+	Encoders enc;
+	Joysticks joy;
+	ElevatorLevel eleVader;
+	MotorSafety safety;
+	MotorSafetyHelper safetyChecker;
+	CameraServer server;
+	
+	Timer stopWatch = new Timer();
+	Timer newStopWatch = new Timer();
+	int run = 0;
+	double prevLeftEncoder = 0, prevRightEncoder = 0;
+
+	//Test Variables
+	boolean driveEncoders, driveCurrent, elevatorCurrent, elevatorEncoder, bannerSensor, currentState, wristEncoder, wristLimitSwitch, wristCurrent, intakeBanner;
+
+	@Override
+	public void robotInit() 
+	{
+		try
+		{
+			CrashChecker.logRobotInit();
+			//server = CameraServer.getInstance();
+			//server.startAutomaticCapture("cam0", 0);
+			enc = new Encoders();
+			safetyChecker = new MotorSafetyHelper(safety);
+			joy = new Joysticks();
+			eleVader = new ElevatorLevel();
+			Encoders.resetEncoders();
+			ElevatorLevel.resetElevatorEncoders();
+			Drivetrain.drivetrainInitialization();
+			setTests();
+			
+		}
+		catch(Throwable t)
+		{
+			CrashChecker.logThrowableCrash(t);
+			throw t;
+		}
+	}
+	
+	public void setTests()
+	{
+		driveEncoders = false;
+		driveCurrent = false;
+		elevatorCurrent = false;
+		elevatorEncoder = false;
+		bannerSensor = false;
+		currentState = false;
+		wristEncoder = false;
+		wristLimitSwitch = false;
+		wristCurrent = false;
+		intakeBanner = false;
+	}
+	
+	@Override
+	public void autonomousInit() 
+	{
+		try 
+		{
+			CrashChecker.logAutoInit();
+			Autonomous.initialize();
+		}
+		catch(Throwable t)
+		{
+			CrashChecker.logThrowableCrash(t);
+			throw t;
+		}	
+	}
+
+	@Override
+	public void autonomousPeriodic() 
+	{
+		while(DriverStation.getInstance().isAutonomous() && !DriverStation.getInstance().isDisabled())
+		{
+			//runMotorSafety();
+			updateJoysticks();
+			enc.setEncoderValues();
+			eleVader.setElevatorEncoder();
+			Autonomous.test(Encoders.leftEncoderValue, Encoders.rightEncoderValue, joy.buttonA);
+			//Autonomous.runAuto(Encoders.leftEncoderValue, Encoders.rightEncoderValue);
+			//runTests();
+		}
+	}
+	
+	@Override
+	public void disabledPeriodic()
+	{
+		Drivetrain.setToCoast();
+	}
+	
+	@Override
+	public void teleopInit()
+	{
+		Drivetrain.setToCoast();
+		Forks.lockTheForks();
+		Shifter.lowGear();
+		Elevator.elevatorState = 0;
+		stopWatch.stop();
+		stopWatch.reset();
+		run = 0;
+	}
+	
+	@Override
+	public void teleopPeriodic() 
+	{
+		try 
+		{
+			CrashChecker.logTeleopPeriodic();
+			updateJoysticks();
+			runMotorSafety();
+			runPistonsandForks();
+			runDrivetrain();
+//			if(prevLeftEncoder == Encoders.leftEncoderValue && run == 0)
+//			{
+//				stopWatch.start();
+//			}
+//			else if(prevLeftEncoder == Encoders.leftEncoderValue)
+//			{
+//				
+//			}
+//			else
+//			{
+//				stopWatch.stop();
+//				System.out.println(stopWatch.get());
+//				Encoders.testEncoders();
+//				prevLeftEncoder = Encoders.leftEncoderValue;
+//				prevRightEncoder = Encoders.rightEncoderValue;
+//				run = 0;
+//			}
+			runElevator();
+			IntakeWheels.runIntake(joy.leftTrigger1, joy.rightTrigger1, false, 0, 0);
+			runWrist();
+			//Lights.runLights();
+			runTests();
+		}
+		catch(Throwable t)
+		{
+			CrashChecker.logThrowableCrash(t);
+			throw t;
+		}
+	}
+
+	@Override
+	public void testInit()
+	{
+		try 
+		{
+			CrashChecker.logAutoInit();
+			Autonomous.initialize();
+		}
+		catch(Throwable t)
+		{
+			CrashChecker.logThrowableCrash(t);
+			throw t;
+		}	
+	}
+	
+	@Override
+	public void testPeriodic() 
+	{
+//		updateJoysticks();
+//		enc.setEncoderValues();
+//		eleVader.setElevatorEncoder();
+//		runPistonsandForks();
+//		IntakeWheels.runIntake(joy.leftTrigger1, joy.rightTrigger1, false, 0, 0);
+//		Elevator.moveEleVader(joy.rightJoySticky * .4);
+//		Drivetrain.tankDrive(joy.leftJoySticky, joy.leftJoySticky);
+//		Lights.runLights();
+//		runTests();
+		runMotorSafety();
+		updateJoysticks();
+		enc.setEncoderValues();
+		eleVader.setElevatorEncoder();
+		Autonomous.test(Encoders.leftEncoderValue, Encoders.rightEncoderValue, joy.buttonA);
+//		Elevator.moveEleVader(joy.rightJoySticky * 1);
+//		Shifter.runPiston(joy.buttonY);
+	}
+	
+	
+	public void updateJoysticks()
+	{
+		joy.setMainContollerValues();
+		joy.setCoDriverContollerValues();
+	}
+	
+	public void runElevator()
+	{
+		eleVader.setElevatorEncoder();
+		if(Shifter.piston.get() == DoubleSolenoid.Value.kReverse)
+		{
+			Elevator.moveEleVader(joy.rightJoySticky1 * 1);
+		}
+		else
+		{
+			Elevator.setElevatorButtons(joy.buttonA1, false, joy.buttonB1,  joy.buttonY1, joy.buttonX1);
+			Elevator.setManualOverride(joy.rightJoySticky1 * .6);
+			Elevator.runDarthVader();
+		}
+	}
+	public void runWrist(){
+		Wrist.setWristEncoder();
+		Wrist.setWristButtons(joy.dPadUp,joy.dPadSide,joy.dPadDown);
+		Wrist.setManualWristOverride(joy.leftJoySticky1 * 0.4);
+		//Wrist.runWrist(joy.leftJoySticky1 * 0.4);
+	}
+
+	public void runPistonsandForks()
+	{
+		Intake.runIntake(joy.rightBumper1);
+		Forks.runPiston(joy.buttonX);
+		Shifter.runPiston(joy.buttonY);
+		TiltServo.PullForks(joy.leftTrigger, joy.rightTrigger);
+		Lock.runPiston(joy.buttonA);
+		Compressor007.runCompressor();
+	}
+	
+	public void runDrivetrain()
+	{
+		enc.setEncoderValues();
+		if(joy.leftBumper)
+		{
+			Drivetrain.arcadeDrive(Encoders.leftEncoderValue, Encoders.rightEncoderValue, joy.leftJoySticky * .45, joy.rightJoyStickx * .5);
+		}
+		else
+		{
+			//Drivetrain.arcadeDrive(Encoders.leftEncoderValue, Encoders.rightEncoderValue, joy.leftJoySticky, joy.rightJoyStickx);
+			Drivetrain.FRCarcadedrive(joy.leftJoySticky, joy.rightJoyStickx);
+		}
+	}
+	
+	public void runMotorSafety()
+	{
+		safetyChecker.setSafetyEnabled(false);
+	}
+	
+	public void runTests()
+	{
+		if(driveEncoders)
+		{
+			Encoders.testEncoders();
+		}
+		if(driveCurrent)
+		{
+			Drivetrain.testDrivetrainCurrent();
+		}
+		if(elevatorCurrent)
+		{
+			Elevator.testElevatorCurrent();
+		}
+		if(bannerSensor)
+		{
+			ElevatorLevel.testBannerSensor();
+		}
+		if(currentState)
+		{
+			System.out.println(Autonomous.currentState);
+		}
+		if(elevatorEncoder)
+		{
+			ElevatorLevel.testElevatorEncoders();
+		}
+		if(wristEncoder){
+			Wrist.testWristEncoder();
+		}
+		if(wristCurrent){
+			Wrist.testWristCurrent();
+		}
+		if(wristLimitSwitch){
+			Wrist.testLimitSwitch();
+		}
+		if(intakeBanner){
+			IntakeWheels.testBannerSensor();
+		}
+	}
+}
---
+++ powerup2018.zip_expanded/Robot_Code/src/team3647elevator/IntakeWheels.java
@@ -0,0 +1,68 @@
+package team3647elevator;
+
+import com.ctre.phoenix.motorcontrol.ControlMode;
+import com.ctre.phoenix.motorcontrol.can.VictorSPX;
+
+import edu.wpi.first.wpilibj.DigitalInput;
+import edu.wpi.first.wpilibj.DoubleSolenoid;
+import team3647ConstantsAndFunctions.Constants;
+import team3647pistons.Intake;
+
+public class IntakeWheels 
+{
+	public static VictorSPX rightIntakeMotor = new VictorSPX(Constants.rightIntakePin);
+	public static VictorSPX leftIntakeMotor = new VictorSPX(Constants.leftIntakePin);
+	
+	public static DigitalInput bannerSensor = new DigitalInput(Constants.intakeBannerSensor);
+	
+	public static void runIntake(double lTrigger, double rTrigger, boolean auto, double lSpeed, double rSpeed)
+	{
+		if(!auto)
+		{
+			if(lTrigger > 0)//pickUp
+			{	
+				rightIntakeMotor.set(ControlMode.PercentOutput, lTrigger *1);
+				leftIntakeMotor.set(ControlMode.PercentOutput, lTrigger *.9);
+			}
+			else if(rTrigger > 0)//shoot
+			{
+				rightIntakeMotor.set(ControlMode.PercentOutput, -rTrigger*1);
+				leftIntakeMotor.set(ControlMode.PercentOutput, -rTrigger*1);
+			}
+		
+			
+		}
+		else
+		{
+			rightIntakeMotor.set(ControlMode.PercentOutput, rSpeed);
+			leftIntakeMotor.set(ControlMode.PercentOutput, lSpeed);
+		}
+	}
+	
+	public static void shoot(double speed)
+	{
+		runIntake(0, 0, true, speed, speed);
+	}
+	
+	public static void pickUp(double speed)
+	{
+		runIntake(0, 0, true, -speed, -speed);
+	}
+	
+	public static void manuallyRunIntake(double lSpeed, double rSpeed)
+	{
+		runIntake(0, 0, true, lSpeed, rSpeed);
+	}
+	
+	public static boolean getIntakeBannerSenor()
+	{
+		return bannerSensor.get();
+	}
+	public static void testBannerSensor(){
+		if(getIntakeBannerSenor()){
+			System.out.println("Intake Banner = True");
+		} else {
+			System.out.println("Intake Banner = False");
+		}
+	}
+}
